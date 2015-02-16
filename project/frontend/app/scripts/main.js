63b9a2db43f6ce223a94c87623728b05e01a760d
Cristián Berríos <cristian.berrios@outlook.cl>
2015-02-16 09:08:05 -0600

    - Style fixes for Chrome - Moved files to source directory - Added Chrome Extension files

--- project/frontend/app/scripts/main.js
+++ project/frontend/app/scripts/main.js
@@ -1,2 +1,20 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(callback) {
+  var action_url = chrome.extension.getURL("main.html");
+  chrome.tabs.create({ url: action_url });
+});
+chrome.storage.onChanged.addListener(function (changes) {
+  chrome.storage.local.get(null, function (storage) {
+    $('.storage').text(JSON.stringify(storage));
+  })
+});
+
+$('.store').click(function(){
+  var key = $('.key').val();
+  var value = $('.value').val();
+  var store = {};
+  store[key]=value;
+  chrome.storage.local.set(store, function(){});
+});
+
+
