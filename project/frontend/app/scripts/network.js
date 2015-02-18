50dfdff0d2365a5fb6601f071b7be688605e9f13
Cristián Berríos <cristian.berrios@outlook.cl>
2015-02-18 00:34:17 -0600

    - Added online status indicator to main page - Added online status checker routines to network.js - Added data sending routines to network.js

--- project/frontend/app/scripts/network.js
+++ project/frontend/app/scripts/network.js
@@ -1,1 +1,67 @@
+var kpn = {
+  status: 'offline',
+  pingServer: 'http://localhost:3000/ping',
+  dataServer: 'http://localhost:3000/data'
+};
 
+kpn.checkOnline = function checkOnline(callback) {
+  'use strict';
+  var status = 'initial';
+  $.get(this.pingServer)
+    .success(function(data) {
+    callback('online');
+    })
+  .fail(function(data) {
+    callback('offline');
+    });
+};
+
+kpn.statusHandler = function statusHandler(status) {
+  "use strict";
+  this.status = status;
+  if(status) {
+    $(document).trigger(status);
+  }
+};
+
+kpn.onlineHandler = function onlineHandler() {
+  "use strict";
+  $('.indicator').css('background','lightgreen');
+  chrome.storage.local.get(null,function(items) {
+    if(Object.keys(items).length > 0) {
+      console.log('sending items: ' + JSON.stringify(items));
+      kpn.sendItems(items);
+    }
+  });
+};
+
+kpn.sendItems = function sendItems(items) {
+  "use strict";
+  var keys = Object.keys(items);
+  keys.forEach(function(key) {
+    kpn.pushItem(key,items[key]);
+  });
+};
+
+kpn.pushItem = function pushItem(key,value) {
+  "use strict";
+  var obj = {};
+  obj.id = key;
+  obj.data = value;
+  $.post(this.dataServer,JSON.stringify(obj))
+    .success(function() {
+      console.log(key + ' was successfully sent');
+      chrome.storage.local.remove(key);
+    })
+    .fail(function() {
+      console.log('Error when sending key:'+key+' value:'+value);
+    });
+};
+
+setInterval(function() {
+  kpn.checkOnline(function(status) {
+    kpn.statusHandler(status);
+  });
+},5000);
+
+$(document).on('online', kpn.onlineHandler);
