4d92d6f8f839aee8649345dbac1af26320692edf
Cristián Berríos <cristian.berrios@outlook.cl>
2015-02-24 18:30:56 -0600

    Merged temp.js with main.js Refactored script files for proper chrome extension functionality Blocked the option to open more than 1 tab Configured tab as pinned.

---
+++ project/frontend/app/scripts/storage.js
@@ -0,0 +1,43 @@
+/**
+ * Created by cristian on 24/02/15.
+ */
+var kpn={};
+
+kpn.store = function store(obj) {
+  'use strict';
+  var context = this;
+  var nameSpace = this.nameSpace;
+  var storageObj = {};
+  chrome.storage.local.get(nameSpace,function(arr) {
+    console.log('arr:' + JSON.stringify(arr));
+    var tmp = arr[nameSpace];
+    tmp.push(obj);
+    storageObj[nameSpace] = tmp;
+    chrome.storage.local.set(storageObj, function() {
+      context.addTotal(obj.saleTotal);
+    });
+  });
+};
+kpn.addTotal = function addTotal(amount) {
+  'use strict';
+  var context = this;
+  var num = Number.parseInt(amount.replace(/[^\d]/g,''));
+  chrome.storage.local.get(['totalAmount','totalForms'],function(data){
+    data.totalAmount= +data.totalAmount + num;
+    data.totalForms = +data.totalForms + 1;
+    chrome.storage.local.set(data,function() {
+      context.updateTotal();
+      return true;
+    });
+  });
+};
+kpn.updateTotal = function updateTotal(){
+  'use strict';
+  chrome.storage.local.get(['totalAmount','totalForms'],function(data) {
+    console.log('data:'+JSON.stringify(data));
+    var totalAmount = data.totalAmount || 0;
+    var totalForms = data.totalForms || 0;
+    $('.feedback-total-amount').text('Rp. ' + totalAmount.toLocaleString('in'));
+    $('.feedback-total-forms').text(totalForms);
+  });
+};
