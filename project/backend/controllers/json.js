58f90bfd520ee65237efb2ebf2aea6b0edf3abeb
Cristián Berríos <cristian.berrios@outlook.cl>
2015-02-24 21:44:40 -0600

    split the project into 2 repositories, for better heroku integration in backend and shorter folder structure.

--- project/backend/controllers/json.js
+++
@@ -1,9 +0,0 @@
-/*
- *
- * Created by cristian on 23/02/15.
- * controller for receiving data from Kopernik's chrome extension
- */
-
-module.exports = function(req, res, next) {
-  res.json(req.body);
-}
